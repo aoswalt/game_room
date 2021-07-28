9befe4e51da8b7754674825d98e49740afc7886e
Adam Oswalt <email@adamoswalt.com>
2021-07-28 15:38:08 -0500

    Set game id in page assigns

--- lib/game_room_web/live/game_live.ex
+++ lib/game_room_web/live/game_live.ex
@@ -2,7 +2,8 @@ defmodule GameRoomWeb.GameLive do
   use GameRoomWeb, :live_view
 
   @impl true
-  def mount(_params, session, socket) do
-    {:ok, assign_defaults(session, socket)}
+  def mount(%{"id" => id}, session, socket) do
+    socket = assign_defaults(session, socket)
+    {:ok, assign(socket, :id, id)}
   end
 end
